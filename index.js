52362a022eccf75e55c7ea73bea709ff056fa340
Karter <kdlmuzixnerd14@gmail.com>
2022-07-07 14:46:52 -0700

    commit tree 0c18f14675f18a46685fd448ff187d28fc78f534

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+// // Add your code here
+
+const url = "http://localhost:3000/users"
+
+function submitData(name, email)
+{
+    const body = {};
+    body.name = name;
+    body.email = email;
+
+    console.log(body.name)
+    console.log(body.email)
+
+    fetch("http://localhost:3000/users",{
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body:JSON.stringify(body)
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(res => alert(res))
+    
+}
+
+function deleteData(id)
+{
+    fetch(`${url}/${id}`,
+    {
+        method: "DELETE",
+        headers:{'Content-Type':'application/json'}
+    })
+    .then(res => res.json())
+}
